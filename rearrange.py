c8bfa3ffc903d1a3d24b1e9351288f2acb56c8ee
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2022-01-21 18:53:03 -0500

    Added rearrange module

---
+++ rearrange.py
@@ -0,0 +1,12 @@
+import random
+import sys
+
+words = [sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]]
+
+def rearrange():
+  random.shuffle(words)
+  print(words[0] + ' ' + words[1] + ' ' + words[2] + ' ' + words[3])
+
+rearrange()
+
+  
