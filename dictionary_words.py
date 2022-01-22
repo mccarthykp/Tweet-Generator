ac9fdf794f16d17ed99a1e032a42753d9a65be1b
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2022-01-21 20:39:39 -0500

    updated dictionary_words.py

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,14 +1,23 @@
 import random, sys
+from unittest import result
 
 def sentence_from_file():
   myfile = open('/usr/share/dict/words')
-  content_of_file = myfile.read()
+  content_of_file = myfile.readlines()
 
-  # push inputted number of random words into words_list
+  num_words = int(sys.argv[1])
+  words_list = []
 
+  for index in range(num_words):
+    # push inputted number of random words into words_list
+    rand_num = random.randint(0, len(content_of_file))
+    words_list.append(content_of_file[rand_num].strip('\n'))
+    
+  result = ' '
+  print(result.join(words_list))
   myfile.close()
 
-words_list = []
+if __name__ == '__main__':
+  sentence_from_file()
 
-sentence_from_file()
-
+# augment function to take in two arguments = the file and the inputted number
--- rearrange.py
+++ rearrange.py
@@ -4,8 +4,8 @@ words = [sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]]
 
 def rearrange():
   random.shuffle(words)
-  print(words[0] + ' ' + words[1] + ' ' + words[2] + ' ' + words[3])
+  # print(words[0] + ' ' + words[1] + ' ' + words[2] + ' ' + words[3])
+  phrase = ' '
+  print(phrase.join(words))
 
 rearrange()
-
-  
